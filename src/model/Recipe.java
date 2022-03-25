eab42998381acd3c9199d32b6c8b3b5d820faf8b
Nate Rardin <nathanaelrardin@gmail.com>
2022-03-25 13:09:36 -0500

    Added attributes to Recipe.java

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -1,6 +1,15 @@
 package model;
-
+import java.util.*;
 
 public class Recipe extends Entity implements Searchable, Categorizable, Exportable {
+
+    private String name;
+    private String description;
+    private Date createdOn;
+    private IngredientList ingredientList;
+    private List<Review> reviews;
+    private List<Category> categories;
+    private List<String> instructions;
+
 
 }
