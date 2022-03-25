a8662ceb848c02539b2a5c2a63262d7d93aed463
Nate Rardin <nathanaelrardin@gmail.com>
2022-03-25 15:53:54 -0500

    Moved method declarations into IngredientList.java. That's where they were supposed to go

--- src/model/IngredientList.java
+++ src/model/IngredientList.java
@@ -1,4 +1,12 @@
 package model;
 public class IngredientList extends Entity implements Exportable {
     
+
+    public void addIngredient(Ingredient ingredient){
+
+    }
+
+    public void removeIngredient(Ingredient ingredient){
+
+    }
 }
