dd7bab74b13aa8ab82ff524780390c3351be8bde
Nate Rardin <nathanaelrardin@gmail.com>
2022-03-25 14:07:57 -0500

    added recipeBuilder nested class

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -12,5 +12,62 @@ public class Recipe extends Entity implements Searchable, Categorizable, Exporta
     private List<Category> categories;
     private List<String> instructions;
 
+    /**
+     * Builder Class for Recipe Objects
+     */
+    public static class RecipeBuilder
+    {
+        private String name;
+        private String description;
+        private int recipeServingSize;
+        private Date createdOn;
+        private IngredientList ingredientList;
+        private List<Review> reviews;
+        private List<Category> categories;
+        private List<String> instructions;
 
+        public RecipeBuilder(){
+            this.createdOn = new Date();
+        }
+
+        public Recipe build(){
+            Recipe recipe = new Recipe(this);
+            return recipe;
+        }
+
+        public RecipeBuilder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public RecipeBuilder setDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public RecipeBuilder setServingSize(int recipeServingSize){
+            this.recipeServingSize = recipeServingSize;
+            return this;
+        }
+
+        public RecipeBuilder setIngredientList(IngredientList ingredientList){
+            this.ingredientList = ingredientList;
+            return this;
+        }
+        
+        public RecipeBuilder setReviews(List<Review> reviews){
+            this.reviews = reviews;
+            return this;
+        }
+        
+        public RecipeBuilder setCategories(List<Category> categories){
+            this.categories = categories;
+            return this;
+        }
+        
+        public RecipeBuilder setInstructions(List<String> instructions){
+            this.instructions = instructions
+            return this;
+        }
+    } 
 }
