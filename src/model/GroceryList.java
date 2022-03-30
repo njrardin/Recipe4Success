e779dae072c0bd1b9acf2b3a8903e4a2cfc38142
Zach <zmplatt@ilstu.edu>
2022-03-29 20:40:45 -0500

    implementd grocerylist class

--- src/model/GroceryList.java
+++ src/model/GroceryList.java
@@ -1,5 +1,17 @@
 package model;
 
 public class GroceryList {
-    
+    private IngredientList ingredientList;
+
+    public GroceryList() {
+        ingredientList = new IngredientList();
+    }
+
+    public IngredientList getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(IngredientList ingredientList) {
+        this.ingredientList = ingredientList;
+    }
 }
