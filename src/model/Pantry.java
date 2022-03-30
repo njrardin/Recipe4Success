1ffebdbb893885bf2f9e6956fae329b1460ba912
Zach <zmplatt@ilstu.edu>
2022-03-29 20:39:25 -0500

    implemented pantry

--- src/model/Pantry.java
+++ src/model/Pantry.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Pantry {
-    
+    private IngredientList ingredientList;
+
+    public Pantry() {
+        ingredientList = new IngredientList();
+    }
+
+    public IngredientList getIngredientList() {
+        return ingredientList;
+    }
+
+    public void setIngredientList(IngredientList ingredientList) {
+        this.ingredientList = ingredientList;
+    }
 }
