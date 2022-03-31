8a2f2b748de001c184a94b199ce20d93bf0e15aa
Nate Rardin <nathanaelrardin@gmail.com>
2022-03-30 19:31:58 -0500

    Updated constructor test for thoroughness

--- recipe4success/src/test/java/it326/r4s/RecipeTest.java
+++ recipe4success/src/test/java/it326/r4s/RecipeTest.java
@@ -3,6 +3,7 @@ package it326.r4s;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.BeforeClass;
+import java.util.ArrayList;
 
 public class RecipeTest {
     
@@ -10,8 +11,15 @@ public class RecipeTest {
 
     @BeforeClass
     public static void setUp(){
-        testingRecipe = new Recipe.RecipeBuilder("Mac and Cheese").setDescription("A yummy meal!").build();
-
+        
+        testingRecipe = new Recipe.RecipeBuilder("Mac and Cheese")
+        .setDescription("A yummy meal!")
+        .setServingSize(2)
+        .setIngredientList(new IngredientList())
+        .setReviews(new ArrayList<Review>())
+        .setCategories(new ArrayList<Category>())
+        .setInstructions(new ArrayList<String>())
+        .build();
     }
 
 
