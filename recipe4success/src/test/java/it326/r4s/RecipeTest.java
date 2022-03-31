8b7f7c0c2809db835c78e0934039871aa1bff186
Nate Rardin <nathanaelrardin@gmail.com>
2022-03-30 19:21:57 -0500

    Fixed setup constructor to instantiate test object

--- recipe4success/src/test/java/it326/r4s/RecipeTest.java
+++ recipe4success/src/test/java/it326/r4s/RecipeTest.java
@@ -2,13 +2,13 @@ package it326.r4s;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class RecipeTest {
     
     static Recipe testingRecipe;
 
-    @Before
+    @BeforeClass
     public static void setUp(){
         testingRecipe = new Recipe.RecipeBuilder("Mac and Cheese").setDescription("A yummy meal!").build();
 
