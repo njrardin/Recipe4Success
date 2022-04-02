7efc612ff3deff62471f7d8a8bdd39da73198008
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-02 14:30:57 -0500

    added dependencies for Ingredient.java from UnitConverter.java

--- recipe4success/src/main/java/it326/r4s/UnitConverter.java
+++ recipe4success/src/main/java/it326/r4s/UnitConverter.java
@@ -1,7 +1,16 @@
 package it326.r4s;
 
+import java.util.ArrayList;
+
 public class UnitConverter {
     public enum Unit{
-        
+        //Fill with types
     }
+
+    static final Unit[] massUnits = {
+        //Fill with types
+    };
+    static final Unit[] volumeUnits = {
+        //Fill with types
+    };
 }
