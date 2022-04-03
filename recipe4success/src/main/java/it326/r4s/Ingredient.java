f83673299ef1e6a1bbe42842c84e2e9717cacd7a
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-02 19:41:12 -0500

    added dependencies for Ingredient.java in UnitConverter.java

--- recipe4success/src/main/java/it326/r4s/Ingredient.java
+++ recipe4success/src/main/java/it326/r4s/Ingredient.java
@@ -1,11 +1,13 @@
 package it326.r4s;
 
+import it326.r4s.UnitConverter.*;
+
 public class Ingredient {
     private FoodItem foodItem;
     private double quantity;
-    private UnitConverter.Unit unit;
+    private Unit unit;
 
-    public boolean changeUnit(){
-        return false;
+    public boolean changeUnit(Unit unit){
+        
     }
 }
--- recipe4success/src/main/java/it326/r4s/UnitConverter.java
+++ recipe4success/src/main/java/it326/r4s/UnitConverter.java
@@ -1,14 +1,28 @@
 package it326.r4s;
 
 public class UnitConverter {
+    
     public enum Unit{
         //Fill with types
     }
 
-    static final Unit[] massUnits = {
+    private final Unit[] massUnits = {
         //Fill with types
     };
-    static final Unit[] volumeUnits = {
+    private final Unit[] volumeUnits = {
         //Fill with types
     };
+
+    //returns double of new units value. returns -1 if unconvertable
+    public double convertUnit(Unit unit1, double unit1Amount, Unit unit2){
+        return 0; //Implement
+    }
+
+    public double convertMass(Unit unit1, double unit1Amount, Unit unit2){
+        return 0; //Implement
+    }
+
+    public double convertVolume(Unit unit1, double unit1Amount, Unit unit2){
+        return 0; //Implement
+    }
 }
