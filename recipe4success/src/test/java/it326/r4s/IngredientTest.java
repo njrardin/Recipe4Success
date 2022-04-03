30a0037f0ffa311a969d1cd49868d2de51549de5
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-02 21:07:16 -0500

    implemented test logic for Ingredient.changeUnit(). (test data not yet added)

--- recipe4success/src/test/java/it326/r4s/IngredientTest.java
+++ recipe4success/src/test/java/it326/r4s/IngredientTest.java
@@ -15,15 +15,32 @@ public class IngredientTest {
     public class testChangeUnit{
     
         @Test
-        public void unitConverted(Unit initialUnit, Unit newUnit, double testQuantity){
+        public void unitConverted(Unit initialUnit, Unit newUnit){
+
+            final double TEST_QUANTITY = 10; //should be irrelevant
+            Ingredient theIngredient = new Ingredient(new FoodItem(), TEST_QUANTITY, initialUnit);
+
+            theIngredient.changeUnit(newUnit);
+
+            Unit returnedUnit = theIngredient.getUnit();
+            
+            assertEquals(newUnit, returnedUnit);
+
+        }
+
+        @Test
+        public void quantityConverted(Unit initialUnit, Unit newUnit, double initialQuantity, double expextedNewQuantity){
 
             Ingredient theIngredient = new Ingredient(new FoodItem(), 10, initialUnit);
 
             theIngredient.changeUnit(newUnit);
 
-
+            double returnedQuantity = theIngredient.getQuantity();
             
+            assertEquals(expextedNewQuantity, returnedQuantity, 0.01);
+
         }
+        
     }
 
 }
