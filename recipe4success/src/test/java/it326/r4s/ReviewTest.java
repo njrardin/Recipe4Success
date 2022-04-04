9c1f411f8e352a39da8811af43e919f54e459d4f
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-03 22:22:25 -0500

    Edited testing to be parameterized

--- recipe4success/src/test/java/it326/r4s/ReviewTest.java
+++ recipe4success/src/test/java/it326/r4s/ReviewTest.java
@@ -1,17 +1,51 @@
 package it326.r4s;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import it326.r4s.Review.Rating;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runners.Parameterized.*;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
 public class ReviewTest {
     
-    @Test
-    public void testGetRatingValue(){
-        Review newReview = new Review(new User(), Rating.ONE);
+    @RunWith(Parameterized.class)
+    public static class testGetRatingValueWrapper{
 
-        int ratingValue = newReview.getRatingValue();        
-        assertEquals(1, ratingValue);
+        private int expectedValue;
+        private Rating ratingEnum; //whether that quantity should return true or false
+
+        //Initializes this class with different specified parameters each time
+        public testGetRatingValueWrapper(int expectedValue, Rating ratingEnum){
+            this.expectedValue = expectedValue;
+            this.ratingEnum = ratingEnum;
+        }
+
+        //The sets of parameters with which testChangeUnitQuantities is initialized. Each set is ran once when the test class is run
+        @Parameters
+        public static Collection getRatingValueData(){
+            return Arrays.asList(new Object[][]{
+                {1, Rating.ONE},
+                {2, Rating.TWO},
+                {3, Rating.THREE},
+                {4, Rating.FOUR},
+                {5, Rating.FIVE}
+            });
+        }
+
+        @Test
+        public void testGetRatingValue(){
+            Review newReview = new Review(new User(), ratingEnum);
+
+            int actualValue = newReview.getRatingValue();        
+            assertEquals(expectedValue, actualValue);
+        }
     }
 }
