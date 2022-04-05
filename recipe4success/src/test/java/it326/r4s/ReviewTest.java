61d0411dbafb75c7c200b131b9aafe28e88e44ba
Zach <zmplatt@ilstu.edu>
2022-04-04 20:24:58 -0500

    Ran a few tests on how the rating is handled

---
+++ recipe4success/src/test/java/it326/r4s/ReviewTest.java
@@ -0,0 +1,34 @@
+package it326.r4s;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.BeforeClass;
+import java.util.ArrayList;
+
+public class ReviewTest {
+
+    static Rating testRating;
+    static User testUser;
+    static int testRatingValue;
+    static Review testReview;
+
+    @BeforeClass
+    public static void before(){
+        testUser = new User();
+        testRating = Rating.Five;
+        testReview = new Review(testRating, testUser);
+        }
+
+    @Test
+    public void testSetRating() {
+        testReview.setRating(Rating.Two);
+        assertEquals(Rating.Two, testReview.getRating());
+    }
+
+    @Test
+    public void testGetRating() {
+        assertEquals(Rating.Two, testReview.getRating());
+        assertEquals(2, testReview.getRating().getValue());
+    }
+
+}
