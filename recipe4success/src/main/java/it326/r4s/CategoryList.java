41a5a583c377194345d6765b4129757981345e95
Zach <zmplatt@ilstu.edu>
2022-04-05 21:21:02 -0500

    changes to make enum static/uppercase

--- recipe4success/src/main/java/it326/r4s/CategoryList.java
+++ recipe4success/src/main/java/it326/r4s/CategoryList.java
@@ -3,42 +3,43 @@ package it326.r4s;
 import java.util.ArrayList;
 import java.util.List;
 
-enum CategoryType { Recipe, FoodItem }
-
 public class CategoryList {
+
+    public static enum CategoryType { RECIPE, FOODITEM }
+
     private static List<Category> recipeCategories = new ArrayList<Category>();
     private static List<Category> foodCategories = new ArrayList<Category>();
 
     private CategoryList(CategoryType categoryType) {
-        if (categoryType == CategoryType.Recipe) {
+        if (categoryType == CategoryType.RECIPE) {
             recipeCategories = new ArrayList<Category>();
         }
-        else if (categoryType == CategoryType.FoodItem) {
+        else if (categoryType == CategoryType.FOODITEM) {
             foodCategories = new ArrayList<Category>();
         }
     }
 
     public static List<Category> getCategories(CategoryType categoryType) {
-        if (categoryType == CategoryType.Recipe) {
+        if (categoryType == CategoryType.RECIPE) {
             return recipeCategories;
         }
-        else if (categoryType == CategoryType.FoodItem) {
+        else if (categoryType == CategoryType.FOODITEM) {
             return foodCategories;
         }
         else return null; // this will never actually happen, just avoiding vs code getting mad at me
     }
 
     public static void addCategoryToList(CategoryType categoryType, Category category) {
-        if (categoryType == CategoryType.Recipe) {
+        if (categoryType == CategoryType.RECIPE) {
             recipeCategories.add(category);
         }
-        else if (categoryType == CategoryType.FoodItem) {
+        else if (categoryType == CategoryType.FOODITEM) {
             foodCategories.add(category);
         }
     }
 
     public static Category getCategory(CategoryType categoryType, String name) {
-        if (categoryType == CategoryType.Recipe) {
+        if (categoryType == CategoryType.RECIPE) {
             for (Category category : recipeCategories) {
                 if (name == category.getName()) {
                     return category;
@@ -49,7 +50,7 @@ public class CategoryList {
             addCategoryToList(categoryType, category);
             return category;
         }
-        else if (categoryType == CategoryType.FoodItem) {
+        else if (categoryType == CategoryType.FOODITEM) {
             for (Category category : foodCategories) {
                 if (name == category.getName()) {
                     return category;
@@ -64,7 +65,7 @@ public class CategoryList {
     }
 
     public boolean removeCategory(CategoryType categoryType, String name) {
-        if (categoryType == CategoryType.Recipe) {
+        if (categoryType == CategoryType.RECIPE) {
             for (Category category : recipeCategories) {
                 if (name == category.getName()) {
                     recipeCategories.remove(category);
@@ -72,7 +73,7 @@ public class CategoryList {
                 }
             }
         }
-        else if (categoryType == CategoryType.FoodItem) {
+        else if (categoryType == CategoryType.FOODITEM) {
             for (Category category : foodCategories) {
                 if (name == category.getName()) {
                     foodCategories.remove(category);
