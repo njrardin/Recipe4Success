27c4c28ea89cb2634d5d543e682b917695bc0d86
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-08 17:31:43 -0500

    Added getAttributeSearchStrings() to Searchable.java interface

--- recipe4success/src/main/java/it326/r4s/Searchable.java
+++ recipe4success/src/main/java/it326/r4s/Searchable.java
@@ -1,5 +1,10 @@
 package it326.r4s;
-public interface Searchable {
-    // TODO #9 - implement categorizable interface
 
+import java.util.Collection;
+
+public interface Searchable {
+    /**
+     * @return - a collection of all attributes in the object that should be searched as strings in priority order (i.e. the most important attribute for searching is item 1 and the least is item N)
+     */
+    public Collection<String> getAttributeSearchStrings();
 }
