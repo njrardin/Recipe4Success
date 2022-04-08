7b82b2b511446fd4475073928c236f2e2cc8f052
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-08 17:56:44 -0500

    Implemented searchFor() (iteration 1)

--- recipe4success/src/main/java/it326/r4s/EntitySearcher.java
+++ recipe4success/src/main/java/it326/r4s/EntitySearcher.java
@@ -1,10 +1,24 @@
 package it326.r4s;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-public interface EntitySearcher {
+public class EntitySearcher {
     
-    public Collection<Searchable> searchFor(Collection<Searchable> collectionToSearch, String searchString);
+    public Collection<Searchable> searchFor(Collection<Searchable> collectionToSearch, String searchString){
+        ArrayList<Searchable> itemsThatPassed = new ArrayList<Searchable>();
+
+        //if item has string in any slot, add it to list (ITERATION 1---Has O(N*M) time complexity)
+        for (Searchable searchable: collectionToSearch){
+            for (String attribute: searchable.getAttributeSearchStrings()){
+                if (attribute.contains(searchString)){
+                    itemsThatPassed.add(searchable);
+                    break;
+                }
+            }
+        }
+        return itemsThatPassed;
+    }
     
-    public Collection<Searchable> searchFor(Collection<Searchable> collectionToSearch, Collection<String> prioritizedSearchItems);
+    // public Collection<Searchable> searchFor(Collection<Searchable> collectionToSearch, Collection<String> prioritizedSearchItems);
 }
