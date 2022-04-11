4d3b5ff0160114ea6dd47f346c99e4b6b4487b58
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-11 16:49:33 -0500

    implemented MealPlanSearch.java

--- recipe4success/src/main/java/it326/r4s/MealPlanSearch.java
+++ recipe4success/src/main/java/it326/r4s/MealPlanSearch.java
@@ -3,11 +3,29 @@ package it326.r4s;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import me.xdrop.fuzzywuzzy.*;
+
 public class MealPlanSearch implements CollectionSearch<MealPlan> {
+    
+    private Collection<MealPlan> mealplans;
+
+    public MealPlanSearch(Collection<MealPlan> mealplans){
+        this.mealplans = mealplans;
+    }
 
     @Override
     public ArrayList<MealPlan> searchFor(String searchString) {
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<MealPlan> itemsThatPassed = new ArrayList<MealPlan>();
+        searchString = searchString.toLowerCase();
+
+        for (MealPlan mealplan: mealplans){
+            if(FuzzySearch.partialRatio(searchString, mealplan.getMealPlanName().toLowerCase()) > 75){
+                itemsThatPassed.add(mealplan);
+            }
+            else if(FuzzySearch.partialRatio(searchString, mealplan.getMealPlanDescription().toLowerCase()) > 75){
+                itemsThatPassed.add(mealplan);
+            }
+        }
+        return itemsThatPassed;
     }
 }
