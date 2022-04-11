6a09393612020275d062b87e32b092d7adafac04
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-11 15:02:03 -0500

    Added method stubs for MealPlanSearch.java

---
+++ recipe4success/src/main/java/it326/r4s/MealPlanSearch.java
@@ -0,0 +1,20 @@
+package it326.r4s;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MealPlanSearch implements CollectionSearch<MealPlan> {
+
+    @Override
+    public ArrayList<MealPlan> searchFor(String searchString) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<MealPlan> searchFor(Collection<String> searchStrings) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
