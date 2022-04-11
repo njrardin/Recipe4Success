07c1b15e1bcf236f968be9d5880bd567e40eb3bf
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-11 15:42:06 -0500

    created .equals() method stub for mealplan

--- recipe4success/src/main/java/it326/r4s/MealPlan.java
+++ recipe4success/src/main/java/it326/r4s/MealPlan.java
@@ -18,5 +18,11 @@ public class MealPlan extends Entity implements Searchable, Categorizable, Expor
         // TODO Auto-generated method stub
         return null;
     }
+
+    @Override
+    public boolean equals(Object obj){
+        // TODO Auto-generated method stub
+        return false;
+    }
     
 }
