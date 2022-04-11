cd9690c99b44d4ca56e55c59433cf01e1bf8a220
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-11 15:54:23 -0500

    Revert "created .equals() method stub for mealplan"
    This reverts commit 07c1b15e1bcf236f968be9d5880bd567e40eb3bf.

--- recipe4success/src/main/java/it326/r4s/MealPlan.java
+++ recipe4success/src/main/java/it326/r4s/MealPlan.java
@@ -18,11 +18,5 @@ public class MealPlan extends Entity implements Searchable, Categorizable, Expor
         // TODO Auto-generated method stub
         return null;
     }
-
-    @Override
-    public boolean equals(Object obj){
-        // TODO Auto-generated method stub
-        return false;
-    }
     
 }
