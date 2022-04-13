690caa4a13001765184dd4e9fe7bbdc3ce0b5404
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-13 15:18:36 -0500

    Added doc comments in MealPlanSearch.java

--- recipe4success/src/main/java/it326/r4s/MealPlanSearch.java
+++ recipe4success/src/main/java/it326/r4s/MealPlanSearch.java
@@ -13,6 +13,14 @@ public class MealPlanSearch implements CollectionSearch<MealPlan> {
         this.mealplans = mealplans;
     }
 
+    /**
+     * Searches for a String and returns an ArrayList of any MealPlans fufilling one of the following search criteria
+     * 1) Name has a FuzzySearch partial ratio > 75
+     * 2) Description FuzzySearch partial ratio > 75
+     * @see https://github.com/xdrop/fuzzywuzzy
+     * @param searchString - the string to search MealPlans for
+     * @return a collection of objects which are considered to fit the search critera given the searchString
+     */
     @Override
     public ArrayList<MealPlan> searchFor(String searchString) {
         ArrayList<MealPlan> itemsThatPassed = new ArrayList<MealPlan>();
@@ -29,6 +37,13 @@ public class MealPlanSearch implements CollectionSearch<MealPlan> {
         return itemsThatPassed;
     }
 
+    /**
+     * Searches for a String and returns an ArrayList of any MealPlans whose recipes fufill the search critera
+     * specified in the class Recipe.java of the same package
+     * @see it326.r4s.Recipe.java
+     * @param searchString - the string to search Mealplan Recipes for
+     * @return a collection of MealPlans which are considered to fit the search critera given the searchString
+     */
     public ArrayList<MealPlan> searchRecipesFor(String searchString){
         ArrayList<MealPlan> itemsThatPassed = new ArrayList<MealPlan>();
         searchString = searchString.toLowerCase();
