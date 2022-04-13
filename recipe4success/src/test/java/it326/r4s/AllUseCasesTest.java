6d02e07cf41b0ce70057a58f01c54b040ac79cfc
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-12 23:15:38 -0500

    labeled the todos in the new test class by name of who is responsible for its completion (not necessarily responsible for the methods that allow it to be carried out; you're just responsible for making sure they get done by whoever is supposed to do them)

--- recipe4success/src/test/java/it326/r4s/AllUseCasesTest.java
+++ recipe4success/src/test/java/it326/r4s/AllUseCasesTest.java
@@ -7,179 +7,179 @@ public class AllUseCasesTest {
     
     @Test
     public void testCreateRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
 
     }
     
     @Test
     public void testEditRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Shu
         assertTrue(false);
 
     }
     
     @Test
     public void testDeleteRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
 
     }
     
     @Test
     public void testRateRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
 
     }
     
     @Test
     public void testAdjustIndividualRecipeServingSize(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Zach
         assertTrue(false);
 
     }
     
     @Test
     public void testSearchRecipeByName(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Shu
         assertTrue(false);
 
     }
     
     @Test
     public void testExportRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
 
     }
     
     @Test
     public void testUploadRecipe(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
 
     }
     
     @Test
     public void testFilterRecipesByCategory(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
 
     }
     
     @Test
     public void testCreateMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
 
     }
     
     @Test
     public void testDeleteMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
 
     }
     
     @Test
     public void testAddRecipeToMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
 
     }
     
     @Test
     public void testRemoveRecipeFromMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Shu
         assertTrue(false);
 
     }
     
     @Test
     public void testAdjustMealPlanServingSize(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Zach
         assertTrue(false);
 
     }
     
     @Test
     public void testSelectActiveMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
 
     }
     
     @Test
     public void testSearchMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
 
     }
     
     @Test
     public void testUploadMealPlan(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
     }
     
     @Test
     public void testAddIngredientsFromMealPlanToGroceryList(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Zach
         assertTrue(false);
     }
     
     @Test
     public void testExportGroceryList(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Alex
         assertTrue(false);
     }
     
     @Test
     public void testAddIngredientToGroceryList(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
     }
     
     @Test
     public void testRemoveIngredientFromGroceryList(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
     }
     
     @Test
     public void testMoveGroceryListIngredientsToPantry(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
     }
     
     @Test
     public void testOrganizeIngredientsInGroceryList(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Nate
         assertTrue(false);
     }
     
     @Test
     public void testAddIngredientToPantry(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Shu
         assertTrue(false);
     }
     
     @Test
     public void testRemoveIngredientFromPantry(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
     }
     
     @Test
     public void testListRecipesSatisfiedByPantry(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Josh
         assertTrue(false);
     }
     
     @Test
     public void testRemoveRecipesIngredientsFromPantry(){
-        //TODO - Build Use Case Test
+        //TODO - Build Use Case Test: Zach
         assertTrue(false);
     }
 
