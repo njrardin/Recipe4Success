35064b66154a5e8edaa34d165e80af22bacedd47
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-13 11:54:01 -0500

    Implemented testSearchRecipesFor_SingleString()

--- recipe4success/src/test/java/it326/r4s/MealPlanSearchTest.java
+++ recipe4success/src/test/java/it326/r4s/MealPlanSearchTest.java
@@ -20,7 +20,7 @@ public class MealPlanSearchTest {
         final String SEARCH_STRING = "ever";
 
         //MealPlan 1: Is valid
-        demoMP = new MealPlan();
+        demoMP = new MealPlan("MP1");
         demoRecipe = new Recipe.RecipeBuilder("demoMP1-demoR1").build();
         demoMP.addMeal(new Meal(demoRecipe, 4));
         demoMP.setMealPlanName("First mealplan");
@@ -30,7 +30,7 @@ public class MealPlanSearchTest {
         inputList.add(demoMP);
 
         //MealPlan 2: Is valid
-        demoMP = new MealPlan();
+        demoMP = new MealPlan("MP2");
         demoRecipe = new Recipe.RecipeBuilder("demoMP2-demoR1").build();
         demoMP.addMeal(new Meal(demoRecipe, 4));
         demoMP.setMealPlanName("Evermore menu");
@@ -40,7 +40,7 @@ public class MealPlanSearchTest {
         inputList.add(demoMP);
 
         //MealPlan 3: Is NOT valid
-        demoMP = new MealPlan();
+        demoMP = new MealPlan("MP3");
         demoRecipe = new Recipe.RecipeBuilder("demoMP3-demoR1").build();
         demoMP.addMeal(new Meal(demoRecipe, 4));
         demoMP.setMealPlanName("Des vacances en France");
@@ -49,7 +49,7 @@ public class MealPlanSearchTest {
         inputList.add(demoMP);
 
         //MealPlan 4: Is valid
-        demoMP = new MealPlan();
+        demoMP = new MealPlan("MP4");
         demoRecipe = new Recipe.RecipeBuilder("demoMP4-demoR1").build();
         demoMP.addMeal(new Meal(demoRecipe, 4));
         demoMP.setMealPlanName("Beverly Hills Mealplan");
@@ -59,7 +59,7 @@ public class MealPlanSearchTest {
         inputList.add(demoMP);
 
         //MealPlan 5: Is NOT valid
-        demoMP = new MealPlan();
+        demoMP = new MealPlan("MP5");
         demoRecipe = new Recipe.RecipeBuilder("demoMP5-demoR1").build();
         demoMP.addMeal(new Meal(demoRecipe, 4));
         demoMP.setMealPlanName("Oh no my ISU mealplan ran out");
@@ -78,6 +78,69 @@ public class MealPlanSearchTest {
 
     @Test
     public void testSearchRecipesFor_SingleString(){
-        
+        //Set up
+        MealPlan demoMP;
+        Recipe demoRecipe;
+        ArrayList<MealPlan> expectedList = new ArrayList<MealPlan>();
+        ArrayList<MealPlan> inputList = new ArrayList<MealPlan>();
+        ArrayList<MealPlan> returnList = new ArrayList<MealPlan>();
+
+        final String SEARCH_STRING = "apple";
+
+        //MealPlan 1: Is valid
+        demoMP = new MealPlan("MP1");
+        demoRecipe = new Recipe.RecipeBuilder("apple pie").build();
+        demoMP.addMeal(new Meal(demoRecipe, 4));
+        demoMP.setMealPlanName("First mealplan");
+        demoMP.setMealPlanDescription("This is my first mealplan ever!");
+
+        expectedList.add(demoMP);
+        inputList.add(demoMP);
+
+        //MealPlan 2: Is valid
+        demoMP = new MealPlan("MP2");
+        demoRecipe = new Recipe.RecipeBuilder("grandma's apple cider").build();
+        demoMP.addMeal(new Meal(demoRecipe, 4));
+        demoMP.setMealPlanName("Evermore menu");
+        demoMP.setMealPlanDescription("A scrumptious fantasy menu.");
+
+        expectedList.add(demoMP);
+        inputList.add(demoMP);
+
+        //MealPlan 3: Is NOT valid
+        demoMP = new MealPlan("MP3");
+        demoRecipe = new Recipe.RecipeBuilder("cupcake thing").build();
+        demoMP.addMeal(new Meal(demoRecipe, 4));
+        demoMP.setMealPlanName("Des vacances en France");
+        demoMP.setMealPlanDescription("Je ne savais pas ce qu'ils avaient alors j'ai cree ce plan de repas ci");
+
+        inputList.add(demoMP);
+
+        //MealPlan 4: Is valid
+        demoMP = new MealPlan("MP4");
+        demoRecipe = new Recipe.RecipeBuilder("title that doesn't have the a word").setDescription("description that says apple").build();
+        demoMP.addMeal(new Meal(demoRecipe, 4));
+        demoMP.setMealPlanName("Beverly Hills Mealplan");
+        demoMP.setMealPlanDescription("I think its clever");
+
+        expectedList.add(demoMP);
+        inputList.add(demoMP);
+
+        //MealPlan 5: Is NOT valid
+        demoMP = new MealPlan("MP5");
+        demoRecipe = new Recipe.RecipeBuilder("wawa we wa").build();
+        demoMP.addMeal(new Meal(demoRecipe, 4));
+        demoMP.setMealPlanName("Oh no my ISU mealplan ran out");
+        demoMP.setMealPlanDescription("guess I gotta buy groceries now");
+
+        inputList.add(demoMP);
+
+
+        MealPlanSearch MealPlanSearch = new MealPlanSearch(new ArrayList<MealPlan>(inputList));
+
+        returnList = MealPlanSearch.searchRecipesFor(SEARCH_STRING);
+
+        assertEquals(expectedList.size(), returnList.size());
+        assertTrue(expectedList.containsAll(returnList));
     }
 }
