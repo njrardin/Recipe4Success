3140c1f5dd7437ef15ee04cc642afaa73bd29d51
Zach <zmplatt@ilstu.edu>
2022-04-14 23:08:26 -0500

    implement use case 28 (remove recipe's ingredients from the pantry)

---
+++ recipe4success/src/test/java/it326/r4s/PantryTest.java
@@ -0,0 +1,56 @@
+package it326.r4s;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import it326.r4s.UnitConverter.Unit;
+
+import org.junit.Before;
+
+public class PantryTest {
+    
+    static Recipe theRecipe;
+    static Ingredient ingredient, ingredient2, ingredient3, ingredient4;
+    static IngredientList ingredientList;
+    static Pantry pantry;
+
+    @Before
+    public void before(){
+
+        theRecipe = new Recipe.RecipeBuilder("Red Velvet Cupcakes")
+        .setDescription("Believe it or not it's actually just red chocolate.")
+        .setIngredientList(new IngredientList())
+        .build();
+
+        ingredient = new Ingredient(new FoodItem("Flour"), 2, Unit.CUP);
+        ingredient2 = new Ingredient(new FoodItem("Egg"), 6, Unit.NONE);
+        ingredient3 = new Ingredient(new FoodItem("Sugar"), 4, Unit.OUNCE);
+        ingredient4 = new Ingredient(new FoodItem("Apple"), 4, Unit.NONE);
+
+        theRecipe.addIngredient(ingredient);
+        theRecipe.addIngredient(ingredient2);
+        theRecipe.addIngredient(ingredient4);
+
+        ingredientList = new IngredientList();
+        ingredientList.addIngredient(ingredient);
+        ingredientList.addIngredient(ingredient2);
+        ingredientList.addIngredient(ingredient3);
+        pantry = new Pantry();
+        pantry.setIngredientList(ingredientList);
+    }
+
+    @Test
+    public void testRemoveRecipeIngredients() {
+        IngredientList expectIngredientList = new IngredientList();
+        expectIngredientList.addIngredient(ingredient3);
+
+        pantry.removeRecipeIngredients(theRecipe);
+        Collection<Ingredient> expectIngredients = expectIngredientList.getIngredients();
+        Collection<Ingredient> actualIngredients = pantry.getIngredientList().getIngredients();
+        assertEquals(expectIngredients, actualIngredients);
+    }
+}
