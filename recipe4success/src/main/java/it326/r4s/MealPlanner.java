9106967af7188214205b95a8895bcae940897045
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-18 15:05:39 -0500

    Revert "add methods to MealPlannner.java" - reverting direct commit to development
    This reverts commit 1a9ebbeb2fb108afc90ce9fe54d8141a0a92af4a.

--- recipe4success/src/main/java/it326/r4s/MealPlanner.java
+++ recipe4success/src/main/java/it326/r4s/MealPlanner.java
@@ -2,53 +2,9 @@ package it326.r4s;
 
 import java.util.*;
 
-import it326.r4s.MealPlan;
-
 /**
  * Meal Planner stores all the meal plans of the system
- * 
- * @author Shu Liao (fliao@ilstu.edu)
- * @date 4/17/2022
  */
 public class MealPlanner extends Entity {
-    private Collection<MealPlan> mealPlans;
-    private int activeMealPlanIndex;
 
-    public MealPlanner() {
-        this.mealPlans = new Collection<MealPlan>();
-        this.activeMealPlanIndex = 0;
-    }
-
-    public MealPlanner(Collection<MealPlan> mealPlans) {
-        this.mealPlans = mealPlans;
-        this.activeMealPlanIndex = 0;
-    }
-
-    public void setActiveMealPlanIndex(int index) {
-        this.activeMealPlanIndex = index;
-    }
-
-    public int getActiveMealPlanIndex() {
-        return this.activeMealPlanIndex;
-    }
-
-    public boolean addMealPlan(MealPlan toAdd) {
-        for (MealPlan meal : mealPlans) {
-            if (meal.equals(toAdd)) {
-                return false;
-            }
-        }
-        mealPlans.add(toAdd);
-        return true;
-    }
-
-    public boolean removeMealPlan(MealPlan toRemove) {
-        for (MealPlan meal : mealPlans) {
-            if (meal.equals(toRemove)) {
-                mealPlans.remove(toRemove);
-                return true;
-            }
-        }
-        return false;
-    }
 }
