5c015bdd26b5e5bfa3627ab11bc060fa034c5848
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-19 12:07:05 -0500

    Changed all instances of CategoryList to CategoryObjPool

--- recipe4success/src/test/java/it326/r4s/CategoryListTest.java
+++ recipe4success/src/test/java/it326/r4s/CategoryListTest.java
@@ -6,26 +6,26 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import it326.r4s.CategoryList.CategoryType;
+import it326.r4s.CategoryObjPool.CategoryType;
 
 public class CategoryListTest {
 
     @BeforeClass
     public static void before() {
-        CategoryList.getCategory(CategoryType.FOODITEM, "Vegetable");
-        CategoryList.getCategory(CategoryType.FOODITEM, "Fruit");
-        CategoryList.getCategory(CategoryType.FOODITEM, "Meat");
+        CategoryObjPool.getCategory(CategoryType.FOODITEM, "Vegetable");
+        CategoryObjPool.getCategory(CategoryType.FOODITEM, "Fruit");
+        CategoryObjPool.getCategory(CategoryType.FOODITEM, "Meat");
     }
 
     @Test
     public void testGetCategory() {
-        Category category = CategoryList.getCategory(CategoryType.FOODITEM, "Fruit");
+        Category category = CategoryObjPool.getCategory(CategoryType.FOODITEM, "Fruit");
         assertEquals("Fruit", category.getName());
     }
 
     @Test
     public void testGetCategories() {
-        List<Category> categories = CategoryList.getCategories(CategoryType.FOODITEM);
+        List<Category> categories = CategoryObjPool.getCategories(CategoryType.FOODITEM);
         assertEquals(categories.get(0).getName(), "Vegetable");
         assertEquals(categories.get(1).getName(), "Fruit");
         assertEquals(categories.get(2).getName(), "Meat");
