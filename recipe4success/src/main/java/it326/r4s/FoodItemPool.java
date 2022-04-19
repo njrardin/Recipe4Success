00640ee1dee66b1fbbfdbcb6cd8b805601af7ec2
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-19 12:48:18 -0500

    implemented method stubs

---
+++ recipe4success/src/main/java/it326/r4s/FoodItemPool.java
@@ -0,0 +1,34 @@
+package it326.r4s;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FoodItemPool {
+    
+    private static HashSet<FoodItem> foodItems;
+
+    public static boolean addFoodItem(FoodItem foodItem){
+        return false;
+    }
+
+    public static boolean addFoodItem(String name){
+        return false;
+    }
+
+    public static boolean removeFoodItem(FoodItem foodItem){
+        return false;
+    }
+
+    public static boolean removeFoodItem(String name){
+        return false;
+    }
+
+    public static boolean hasFoodItem(FoodItem foodItem){
+        return false;
+    }
+
+    public static FoodItem getFoodItem(String name){
+        return null;
+    }
+}
