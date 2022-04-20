c5c3c1e4d810c4355ba745af33b230664db4eef8
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-19 20:00:50 -0500

    added method comment

--- recipe4success/src/main/java/it326/r4s/FoodItemPool.java
+++ recipe4success/src/main/java/it326/r4s/FoodItemPool.java
@@ -5,6 +5,12 @@ public class FoodItemPool {
 
     private static Hashtable<String, FoodItem> foodItems;
 
+    /**
+     * Gets the food item from the pool with the appropriate name. 
+     * If this food item does not exist, then it creates one, adds it to the pool, and returns it to the user.
+     * @param name of the food item as a string
+     * @return the food item from the pool with that name
+     */
     public static FoodItem getFoodItem(String name){
         if(!foodItems.containsKey(name)){
             foodItems.put(name, new FoodItem(name));
