0023f6f21bfccaf69f93cf682ce46e56902dda7e
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-19 20:48:22 -0500

    test for removeFoodItem

--- recipe4success/src/test/java/it326/r4s/FoodItemPoolTest.java
+++ recipe4success/src/test/java/it326/r4s/FoodItemPoolTest.java
@@ -1,6 +1,10 @@
 package it326.r4s;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,5 +13,18 @@ public class FoodItemPoolTest {
     @Test
     public void testGetFoodItem(){
         assertEquals("egg", FoodItemPool.getFoodItem("egg").getName());
+    }
+
+    @Test
+    public void testGetAllFoodItems(){
+        assertNotNull(FoodItemPool.getAllFoodItems());
+    }
+
+    @Test
+    public void testRemoveFoodItem(){
+        FoodItemPool.getFoodItem("cheese");
+
+        assertTrue(FoodItemPool.removeFoodItem("cheese"));
+        assertFalse(FoodItemPool.removeFoodItem("cheese"));
     }
 }
