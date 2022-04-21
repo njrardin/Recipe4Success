90fe684cfeaaddb17aa8e743b1b30a5065bdb08f
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-20 22:40:10 -0500

    Added ability to exit

--- recipe4success/src/main/java/it326/r4s/view/MainMenu.java
+++ recipe4success/src/main/java/it326/r4s/view/MainMenu.java
@@ -7,10 +7,10 @@ public class MainMenu {
     public static void executeMainMenu(){
         System.out.println("");
         System.out.println("Please type the number corresponding to the option you wish to select:");
-        System.out.println("1) Open my RecipeBook:\tsearch, edit, or create new tasty recipes!");
-        System.out.println("2) Open my MealPlanner:\torganize your recipes into comprehensive meal plans.");
-        System.out.println("3) Open my Pantry:\torganize your recipes into comprehensive meal plans.");
-        System.out.println("4) Access my grocery list:\tview and edit your current grocery list.");
+        System.out.println("1) Open my RecipeBook:\t\t\tsearch, edit, or create new tasty recipes!");
+        System.out.println("2) Open my MealPlanner:\t\t\torganize your recipes into comprehensive meal plans.");
+        System.out.println("3) Open my Pantry:\t\t\torganize your recipes into comprehensive meal plans.");
+        System.out.println("4) Access my grocery list:\t\tview and edit your current grocery list.");
         System.out.println("");
         System.out.println("(If you would like to exit the application, type \"stop\")");
         System.out.println();
@@ -49,5 +49,7 @@ public class MainMenu {
  
         System.out.println("Thank you for using Recipe4Success!");
         System.out.println("\n\tapplication exiting...\n\n");
+
+        System.exit(0);
      }
 }
