262b46e542e065811d77011a88155cb2a3d44605
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-20 20:37:56 -0500

    added main menu execution in main welcome screen execution

--- recipe4success/src/main/java/it326/r4s/view/WelcomeScreen.java
+++ recipe4success/src/main/java/it326/r4s/view/WelcomeScreen.java
@@ -3,6 +3,8 @@ package it326.r4s.view;
 public class WelcomeScreen {
     
     public static void executeWelcomeScreen(){
-        
+        //TODO: Implement this
+
+        MainMenu.executeMainMenu();
     }
 }
