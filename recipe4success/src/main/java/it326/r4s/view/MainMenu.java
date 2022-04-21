db5ff0f58331228552238235bf8af4e833121c82
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-20 22:18:00 -0500

    added main menu options

--- recipe4success/src/main/java/it326/r4s/view/MainMenu.java
+++ recipe4success/src/main/java/it326/r4s/view/MainMenu.java
@@ -3,6 +3,14 @@ package it326.r4s.view;
 public class MainMenu {
     
     public static void executeMainMenu(){
-        
+        System.out.println("");
+        System.out.println("Please type the number corresponding to the option you wish to select:");
+        System.out.println("1) Open my RecipeBook:\tsearch, edit, or create new tasty recipes!");
+        System.out.println("2) Open my MealPlanner:\torganize your recipes into comprehensive meal plans.");
+        System.out.println("3) Open my Pantry:\torganize your recipes into comprehensive meal plans.");
+        System.out.println("4) Access my grocery list:\tview and edit your current grocery list.");
+        System.out.println("");
+        System.out.println("(If you would like to exit the application, type \"stop\")");
+        System.out.println();
     }
 }
