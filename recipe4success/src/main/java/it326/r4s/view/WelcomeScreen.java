984317a5dc8c9058cff99d49de6d74f7d31163bb
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-20 22:17:30 -0500

    implemented welcome screen visual

--- recipe4success/src/main/java/it326/r4s/view/WelcomeScreen.java
+++ recipe4success/src/main/java/it326/r4s/view/WelcomeScreen.java
@@ -3,8 +3,20 @@ package it326.r4s.view;
 public class WelcomeScreen {
     
     public static void executeWelcomeScreen(){
-        //TODO: Implement this
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("---                                                                               ---");
+        System.out.println("---                      -- WELCOME TO RECIPES 4 SUCCESS! --                      ---");
+        System.out.println("---                                                                               ---");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        welcomeSetup();
 
         MainMenu.executeMainMenu();
+    }
+
+    private static void welcomeSetup(){
+        //TODO: Implement the logic for importing all the items and stuff 
     }
 }
