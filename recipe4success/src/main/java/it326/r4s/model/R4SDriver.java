96b0ba8377ff039e762a60d5c920b9d44a63b0af
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-24 14:45:32 -0500

    initialization of user

--- recipe4success/src/main/java/it326/r4s/model/R4SDriver.java
+++ recipe4success/src/main/java/it326/r4s/model/R4SDriver.java
@@ -7,8 +7,13 @@ import it326.r4s.view.WelcomeScreen;
 
 public class R4SDriver {
 
+    public static User theUser = new User("");
+
     public static void main(String[] args) {
         importFileData();
+        
+        theUser.setName("Example Name");; //TODO: Some way to get the user's name and other info the first time they use the app and remember it
+        
         WelcomeScreen.executeWelcomeScreen();
     }
     //     System.out.println();
