a1171079648886b774caa39ebd7809886ef6caef
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-24 14:09:43 -0500

    implemented loop of recipe book view

--- recipe4success/src/main/java/it326/r4s/view/RecipeBookView.java
+++ recipe4success/src/main/java/it326/r4s/view/RecipeBookView.java
@@ -1,10 +1,84 @@
 package it326.r4s.view;
 
+import java.util.Scanner;
+
+import it326.r4s.model.*;
+
 public class RecipeBookView {
     
     public static void ExecuteRecipeBookView(){
 
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("---                                                                               ---");
+        System.out.println("---                               -- Recipe Book --                               ---");
+        System.out.println("---                                                                               ---");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
+        
 
+        
+    }
+
+    private static void displayOptions(){
+        System.out.println("");
+        System.out.println("                               -- Recipe Book Options --                              ");
+        System.out.println("");
+        System.out.println("1) Search and filter recipes");
+        System.out.println("2) Import a recipe");
+        System.out.println("3) Export a recipe");
+        System.out.println("4) Create a new recipe");
+        System.out.println("");
+        System.out.println("(If you would like to go back to the main menu, type \"back\")");
+        System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.print("Please type the number corresponding to ythe option you wish to select: ");
+
+            input = scan.nextLine().toLowerCase();
+            System.out.println();
+            switch (input) {
+                case "1":
+                    RecipeBookView.ExecuteRecipeBookView();
+                    break;
+                case "2":
+                    MealPlannerView.ExecuteMealPlannerView();
+                    break;
+                case "3":
+                    PantryView.ExecutePantryView();
+                    break;
+                case "4":
+                    GroceryListView.ExecuteGroceryListView();
+                    break;
+                case "back":
+                    break;
+                default:
+                    System.out.println("Invalid input, please try again\n");
+            }
+            break;
+        }
+        scan.close();
+    }
+
+    private static void displayOneline(Recipe recipe){
+
+    }
+    
+    private static void createRecipe(){
+
+    }
+    
+    private static void searchRecipes(){
+        
+    }
+    
+    private static void exportRecipe(){
+        
+    }
+    
+    private static void importRecipe(){
         
     }
 }
