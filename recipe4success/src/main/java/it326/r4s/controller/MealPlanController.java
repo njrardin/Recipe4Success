3484be048938ed42be53ef1417a27793fc7fe062
Zach <zmplatt@ilstu.edu>
2022-04-26 18:18:38 -0500

    created requirement method stubs for mealplan

--- recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
+++ recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
@@ -2,4 +2,27 @@ package it326.r4s.controller;
 
 public class MealPlanController {
     
+    public void addRecipeToMP() {
+        //TODO - req 12
+    }
+
+    public void removeRecipeFromMP() {
+        //TODO - req 13
+    }
+
+    public void setMealPlanServingSize() {
+        //TODO - req 14
+    }
+    
+    public void markAsActive() {
+        //TODO - req 15
+    }
+
+    public void addAllIngredientsToGroceryList() {
+        //TODO - req 19
+    }
+
+    public void deleteMealPlan() {
+        //TODO - req 11
+    }
 }
