60c5fd330bb4d955145af667dd1e745f831ea7a0
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-26 00:39:43 -0500

    scaffolded beginnings of RecipeBookView

--- recipe4success/src/main/java/it326/r4s/view/RecipeBookView.java
+++ recipe4success/src/main/java/it326/r4s/view/RecipeBookView.java
@@ -1,46 +1,46 @@
 package it326.r4s.view;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
-import it326.r4s.model.*;
-import it326.r4s.model.Recipe.RecipeBuilder;
-
-public class RecipeBookView {
+import it326.r4s.controller.RecipeBookController;
+public class RecipeBookView implements CLI_View{
     
-    public static void ExecuteRecipeBookView(Scanner scan){
-        
-        System.out.println("-------------------------------------------------------------------------------------");
-        System.out.println("-------------------------------------------------------------------------------------");
-        System.out.println("---                                                                               ---");
-        System.out.println("---                               -- Recipe Book --                               ---");
-        System.out.println("---                                                                               ---");
-        System.out.println("-------------------------------------------------------------------------------------");
-        System.out.println("-------------------------------------------------------------------------------------");
-        
-        String input = "";
-        while (true) {
-            displayOptions();
+    private RecipeBookController rbController;
 
+    public RecipeBookView(RecipeBookController rbController){
+        this.rbController = rbController;
+    }
+
+    public void execute(){
+        String input = "";
+        Scanner scan = ViewUtilities.scan;
+
+        displayHeader();
+        displayOptions();
+        while (true) {
             System.out.print("Please type the number corresponding to the option you wish to select: ");
-            System.out.println("(to see the options again, type \"options\"");
+            System.out.println("(to see the options again, type \"options\")");
     
             input = scan.nextLine().toLowerCase();
             System.out.println();
             switch (input) {
                 case "1":
-                    RecipeBookView.searchRecipes(scan);
+                    rbController.searchRecipes();
                     break;
                 case "2":
-                    RecipeBookView.importRecipe(scan);
+                    rbController.importRecipe();
                     break;
                 case "3":
-                    RecipeBookView.exportRecipe(scan);
+                    rbController.exportRecipe();
                     break;
                 case "4":
-                    Recipe newRecipe = RecipeBookView.createRecipe(scan);
-                    RecipeView.executeRecipeView(newRecipe);
+                    rbController.createRecipe();
                     break;
+                case "5":
+                    rbController.viewRecipes();
+                    rbController.selectRecipe();
+                    break;
+                case "6":
+                    return;
                 case "options":
                     displayOptions();
                 case "back":
@@ -48,7 +48,18 @@ public class RecipeBookView {
                 default:
                     System.out.println("Invalid input, please try again\n");
             }
+            displayOptions();
         }
+    }
+
+    public void displayHeader(){
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("---                                                                               ---");
+        System.out.println("---                               -- Recipe Book --                               ---");
+        System.out.println("---                                                                               ---");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------");
     }
 
     private static void displayOptions(){
@@ -59,130 +70,9 @@ public class RecipeBookView {
         System.out.println("2) Import a recipe");
         System.out.println("3) Export a recipe");
         System.out.println("4) Create a new recipe");
-        System.out.println("");
-        System.out.println("(If you would like to go back to the main menu, type \"back\")");
+        System.out.println("5) View all recipes");
+        System.out.println("6) Go back");
         System.out.println();
-    }
-
-    private static void displayOneline(Recipe recipe){
-    
-    }
-    
-    private static void searchRecipes(Scanner scan){
-        //use displayOneline method
-    }
-    
-    
-    private static void exportRecipe(Scanner scan){
-        
-    }
-    
-    private static void importRecipe(Scanner scan){
-        
-    }
-
-    private static Recipe createRecipe(Scanner scan){
-        
-        String name = "";
-        String description;
-        int servingSize;
-        ArrayList<String> instructions = new ArrayList<String>();
-
-        String resp;
-        
-        while(true){
-            System.out.println("\nPlease provide the recipe's name:");
-            name = scan.nextLine();
-            if(name != ""){
-                System.out.println("You provided the name \"" + name + ",\" is this correct? (Y/N)");
-                if(scan.nextLine().toLowerCase().equals("y")){
-                    break;
-                }
-            }
-        }
-
-        RecipeBuilder rpBuild = new RecipeBuilder(name);
-
-        while(true){
-            System.out.println("Please provide a description for the recipe");
-            description = scan.nextLine();
-
-            System.out.println("You provided the description\n\n \"" 
-
-            + description + 
-
-            "\"\n\n is this correct? (Y/N)");
-
-            if(scan.nextLine().toLowerCase().equals("y")){
-                rpBuild.setDescription(description);
-                break;
-            }
-        }
-        
-        while(true){
-            System.out.println("How many people does this recipe serve?");
-            try{
-                servingSize = Integer.parseInt(scan.nextLine());
-            } catch (NumberFormatException e) {
-                continue;
-            }
-        
-            System.out.println("You provided a serving-size of \"" + servingSize + "\" is this correct? (Y/N)");
-            if(scan.nextLine().toLowerCase().equals("y")){
-                rpBuild.setServingSize(servingSize);
-                break;
-            }
-        }
-
-        String instructionString;
-        int stepNum = 1;
-        System.out.println("Now let's write the instruction steps for the recipe.");
-        while(true){
-            if(stepNum == 1){
-                System.out.println("What is the first step in creating the recipe?\n");
-            }
-            else{
-                System.out.println("What is the next step in creating the recipe?\n");
-            }
-
-            instructionString = scan.nextLine();
-            
-            System.out.println("You provided step #" + stepNum + " as\n\n \"" 
-
-            + instructionString + 
-
-            "\"\n\n is this correct? (Y/N)");
-            resp = scan.nextLine().toLowerCase();
-            if(resp.equals("y")){
-                instructions.add(instructionString);
-                stepNum++;
-            }
-            else{
-                continue;
-            }
-
-            do {
-                System.out.println("Would you like to add another step? (Y/N)");
-                resp = scan.nextLine().toLowerCase();
-            } while (!(resp.equals("y") || resp.equals("n")));
-            
-            if(resp.equals("n")){
-                rpBuild.setInstructions(instructions);
-                break;
-            }
-
-        }
-
-        //TODO: Implement adding cateogries
-        //TODO: Implement adding ingredients
-
-        Recipe newRecipe = rpBuild.build();
-
-        System.out.println("Perfect! Your recipe is complete.");
-        User theUser = User.getUser();
-        theUser.addRecipe(newRecipe);
-
-        return newRecipe;
     }
 
 }
