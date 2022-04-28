d4de89194f8a80d64e4d92cc87eec6f1d710b2cc
Alex Smith <alex1000242@gmail.com>
2022-04-27 21:22:55 -0500

    Resolved errors regarding CategoryPool

--- recipe4success/src/test/java/it326/r4s/CategoryPoolTest.java
+++ recipe4success/src/test/java/it326/r4s/CategoryPoolTest.java
@@ -1,35 +1,25 @@
 package it326.r4s;
 
 import static org.junit.Assert.assertEquals;
-import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import it326.r4s.model.Category;
-import it326.r4s.model.CategoryPool;
-import it326.r4s.model.CategoryPool.CategoryType;
 
 public class CategoryPoolTest {
+    static Category.Pool pool = Category.Pool.getInstance();
 
     @BeforeClass
     public static void before() {
-        CategoryPool.getCategory(CategoryType.FOODITEM, "Vegetable");
-        CategoryPool.getCategory(CategoryType.FOODITEM, "Fruit");
-        CategoryPool.getCategory(CategoryType.FOODITEM, "Meat");
+        pool.getCategory(Category.Type.FOODITEM, "Vegetable");
+        pool.getCategory(Category.Type.FOODITEM, "Fruit");
+        pool.getCategory(Category.Type.FOODITEM, "Meat");
     }
 
     @Test
     public void testGetCategory() {
-        Category category = CategoryPool.getCategory(CategoryType.FOODITEM, "Fruit");
+        Category category = pool.getCategory(Category.Type.FOODITEM, "Fruit");
         assertEquals("Fruit", category.getName());
-    }
-
-    @Test
-    public void testGetCategories() {
-        List<Category> categories = CategoryPool.getCategories(CategoryType.FOODITEM);
-        assertEquals(categories.get(0).getName(), "Vegetable");
-        assertEquals(categories.get(1).getName(), "Fruit");
-        assertEquals(categories.get(2).getName(), "Meat");
     }
 }
