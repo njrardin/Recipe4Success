2fef5101e9e6aad648340fd2d30875ac8b0357be
Alex Smith <alex1000242@gmail.com>
2022-04-27 20:40:52 -0500

    Moved FoodItemPool into FoodItem

--- recipe4success/src/main/java/it326/r4s/model/FoodItem.java
+++ recipe4success/src/main/java/it326/r4s/model/FoodItem.java
@@ -1,5 +1,9 @@
 package it326.r4s.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * The Recipe object class for the Recipe4Success application
  * @author Nate Rardin (njrardi@ilstu.edu)
@@ -8,9 +12,10 @@ package it326.r4s.model;
 public class FoodItem extends Entity {
     private String name;
 
-    public FoodItem(String name){
+    private FoodItem() {}
+
+    private FoodItem(String name){
         this.name = name;
-        FoodItemPool.addFoodItem(this);
     }
 
     public String getName(){
@@ -37,5 +42,53 @@ public class FoodItem extends Entity {
 
         FoodItem otherFoodItem = (FoodItem) obj;
         return this.name.equals(otherFoodItem.getName());
+    }
+
+    /**
+     * A singleton pool of available food items.
+     * 
+     * @author Alex Smith (alsmi14@ilstu.edu)
+     * @date 4/27/22
+     */
+    public static class Pool {
+        private static Pool pool = null;
+        private Collection<FoodItem> foodItems;
+
+        private Pool() {
+            foodItems = new ArrayList<>();
+        }
+
+        public static Pool getInstance() {
+            if (pool == null) {
+                pool = new Pool();
+            }
+
+            return pool;
+        }
+
+        public FoodItem getFoodItem(String name) {
+            for (FoodItem fi : foodItems) {
+                if (fi.getName().equals(name)) {
+                    return fi;
+                }
+            }
+
+            return addFoodItem(name);
+        }
+
+        public boolean removeFoodItem(String name) {
+            FoodItem fi = new FoodItem(name);
+            return foodItems.remove(fi);
+        }
+
+        public Collection<FoodItem> getFoodItems() {
+            return Collections.unmodifiableCollection(foodItems);
+        }
+
+        private FoodItem addFoodItem(String name) {
+            FoodItem fi = new FoodItem(name);
+            foodItems.add(fi);
+            return fi;
+        }
     }
 }
