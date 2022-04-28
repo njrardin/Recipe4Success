f82999d48b053eb19bc445a9a61b14d9f0911d69
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-28 15:13:45 -0500

    Re-added unit MVC

--- recipe4success/src/main/java/it326/r4s/view/UnitView.java
+++ recipe4success/src/main/java/it326/r4s/view/UnitView.java
@@ -2,16 +2,39 @@ package it326.r4s.view;
 
 import java.util.Scanner;
 
-import it326.r4s.model.UnitConverter.Unit;
 
 public class UnitView {
     
-    public static Unit getUnitFromUser(){
-        String unitName = "";
+
+    public static int getUnitSelection() {
+        System.out.println("Please select a unit of measure from the list:");
+        System.out.println("");
+        System.out.println("1) None");
+        System.out.println("2) Teaspoon");
+        System.out.println("3) Tablespoon");
+        System.out.println("4) Fluid Ounce");
+        System.out.println("5) Cup");
+        System.out.println("6) Pint");
+        System.out.println("7) Quart");
+        System.out.println("8) Gallon");
+        System.out.println("9) Milliliter");
+        System.out.println("10) Liter");
+        System.out.println("11) Pound");
+        System.out.println("12) Ounce");
+        System.out.println("13) Milligram");
+        System.out.println("14) Gram");
+        System.out.println("15) Kilogram");
+
         Scanner scan = ViewUtilities.scan;
-        Unit selectedUnit = Unit.NONE;
-
-
-        return selectedUnit; //TODO: this
+        int selection = -1;
+        do{
+            try{
+                selection = Integer.parseInt(scan.nextLine());
+            } catch (Exception e) {
+                System.out.println("Please select an option by typing the corresponding number");
+                continue;
+            }
+        } while(selection >= 15 && selection <=1);
+        return selection;
     }
 }
