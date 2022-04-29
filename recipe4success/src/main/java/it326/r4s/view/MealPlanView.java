0d766b6b37c428b0e613d973c72c4a53e2cd7f11
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-29 00:39:04 -0500

    reverted serving size change

--- recipe4success/src/main/java/it326/r4s/view/MealPlanView.java
+++ recipe4success/src/main/java/it326/r4s/view/MealPlanView.java
@@ -31,11 +31,6 @@ public class MealPlanView implements CLI_Menu {
         System.out.println("-------------------------------------------------------------------------------------");
         System.out.println("Name: " + mealPlanController.getMealPlan().getMealPlanName());
         System.out.println("Description: " + mealPlanController.getMealPlan().getMealPlanDescription());
-        if(mealPlanController.getMealPlan().getServingSize() == 1){
-            System.out.println("Serving size: " + mealPlanController.getMealPlan().getServingSize() + " person");
-        } else {
-            System.out.println("Serving size: " + mealPlanController.getMealPlan().getServingSize() + " people");
-        }
         System.out.println("Created on: " + mealPlanController.getMealPlan().getMealPlanDate());
         System.out.println();
         System.out.println("Recipes: ");
