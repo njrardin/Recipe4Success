51f3f5bbcbf4443c307de1ca7fd5d81e5a6b79da
Nate Rardin <nathanaelrardin@gmail.com>
2022-04-28 23:19:29 -0500

    implemented openMealPlan & relevant method stubs

--- recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
+++ recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
@@ -1,14 +1,12 @@
 package it326.r4s.controller;
 
-import java.util.ArrayList;
 
 import it326.r4s.model.MealPlan;
-import it326.r4s.model.MealPlanner;
 import it326.r4s.view.MealPlanView;
 
 /**
  * Controller for R4S MealPlan
- * @author Zach Plattner (zmplatt@ilstu.edu)
+ * @author Zach Plattner (zmplatt@ilstu.edu) and Nate Rardin (njrardi@ilstu.edu)
  * @date 4/26/22
  */
 public class MealPlanController  {
@@ -17,44 +15,85 @@ public class MealPlanController  {
     public MealPlan mealPlan;
     public MealPlanView mealPlanView;
 
+    //*Constructor*\\
+    /**
+     * Constructor for R4S's MealPlan
+     * @param mealPlan - the controller's MealPlan
+     */
     public MealPlanController(MealPlan mealPlan) {
         this.mealPlan = mealPlan;
         this.mealPlanView = new MealPlanView(this);
     }
 
+    //*Methods*\\
+    /**
+     * Getter for the controller's MealPlanView
+     * @return the MealPlanView object
+     */
     public MealPlanView getMealPlanView() {
         return mealPlanView;
     }
 
-    public void executeView(){
-        mealPlanView.execute();
-    }
+    /**
+     * Launches the mealPlanView to get a user option selection
+     * and then takes the appropriate action
+     */
+    public void openMealPlan(){
+        mealPlanView.displayMealPlan();
+        int option;
+        while(true){
+            option = mealPlanView.getMenuOptionSelection();
 
-    public void launchMealPlanMenu(){
-    
+            switch (option) {
+                case 1:
+                    addRecipeToMealPlan();
+                    break;
+                case 2:
+                    removeRecipeFromMealPlan();
+                    break;
+                case 3:
+                    adjustMealPlanServingSize();
+                    break;
+                case 4:
+                    addToGroceryList();
+                    break;
+                case 5:
+                    exportMealPlan();
+                    return;
+                case 6:
+                    deleteMealPlan();
+                    break;
+                case 7:
+                    return;
+                default:
+                    System.out.println("Invalid input, please try again\n");
+            }
+            mealPlanView.displayMealPlan();
+        }
     }
     
     public MealPlan getMealPlan() {
         return this.mealPlan;
     }
 
-    public void addRecipeToMP() {
+    public void addRecipeToMealPlan() {
         //TODO - req 12
     }
 
-    public void removeRecipeFromMP() {
+    public void removeRecipeFromMealPlan() {
         //TODO - req 13
     }
 
-    public void setMealPlanSS(int newServingSize) { //req 14
-        mealPlan.setMealServingSize(newServingSize);
+    public void adjustMealPlanServingSize() {
+        //TODO - req 14
     }
 
     public void exportMealPlan() {
         //TODO - req 17 @Alex!
+        //  /\ "Lmao" - Nate
     }
 
-    public void addAllIngredientsToGroceryList() {
+    public void addToGroceryList() {
         //TODO - req 19
     }
 
