4346264283db9dd584833c7110d7b098161bf931
Fanshu Liao <s974249831@gmail.com>
2022-04-30 17:33:08 -0500

    Add FoodItemTest to tests

--- recipe4success/src/test/java/it326/r4s/FoodItemTest.java
+++ recipe4success/src/test/java/it326/r4s/FoodItemTest.java
@@ -12,12 +12,52 @@ import java.util.Collection;
 import java.util.Collections;
 
 public class FoodItemTest {
-    static String name;
-    static FoodItem theFood;
+    private static FoodItem.Pool pool1 = null;
+    private static FoodItem.Pool pool2 = null;
+    private Collection<FoodItem> foodItems;
 
     @Before
     public void setUp() {
-        name = "Test Food";
+        pool1 = FoodItem.Pool.getInstance();
+        pool1.getFoodItem("Apple");
+        pool1.getFoodItem("Banana");
+        pool1.getFoodItem("Cherry");
+
+        pool2 = FoodItem.Pool.getInstance();
+        pool2.getFoodItem("Apple");
+        pool2.getFoodItem("Banana");
+        pool2.getFoodItem("Cherry");
+    }
+
+    @Test
+    public void testGetFoodItems() {
+        foodItems = pool1.getFoodItems();
+        ArrayList<FoodItem> actualFoodItems = new ArrayList<FoodItem>();
+        for (FoodItem fi : foodItems) {
+            actualFoodItems.add(fi);
+        }
+        Collection<FoodItem> foodItems2;
+        foodItems2 = pool2.getFoodItems();
+        ArrayList<FoodItem> expectedFoodItems = new ArrayList<FoodItem>();
+        for (FoodItem fi : foodItems) {
+            expectedFoodItems.add(fi);
+        }
+        assertEquals(expectedFoodItems, actualFoodItems);
+    }
+
+    @Test
+    public void testRemoveFoodItem() {
+        assertEquals(true, pool1.removeFoodItem("Banana"));
+        assertEquals(false, pool1.removeFoodItem("Banana"));
+        assertEquals(false, pool1.getFoodItems() == pool2.getFoodItems());
+    }
+
+    @Test
+    public void testContains() {
+        assertEquals(true, pool1.contains("Apple"));
+        pool1.removeFoodItem("Apple");
+        assertEquals(false, pool1.contains("Apple"));
+        assertEquals(false, pool1.contains("Grape"));
     }
 
 }
