e261e31dd63056ce262ad9b6b343ffd00fe8ad6d
Alex Smith <alex1000242@gmail.com>
2022-05-01 02:21:05 -0500

    rough implementation of persistent data

--- recipe4success/src/main/java/it326/r4s/driver/R4SDriver.java
+++ recipe4success/src/main/java/it326/r4s/driver/R4SDriver.java
@@ -2,7 +2,12 @@ package it326.r4s.driver;
 
 import it326.r4s.controller.MainMenuController;
 import it326.r4s.controller.UserController;
+import it326.r4s.model.Category;
+import it326.r4s.model.CategoryPoolManager;
+import it326.r4s.model.FoodItem;
+import it326.r4s.model.FoodItemPoolManager;
 import it326.r4s.model.User;
+import it326.r4s.model.UserManager;
 import it326.r4s.view.utilities.DisplayUtils;
 import it326.r4s.view.utilities.InputAccess;
 public class R4SDriver {
@@ -10,24 +15,27 @@ public class R4SDriver {
         
         displayWelcome();
 
-        importFileData(); //TODO: Some way to get the user's name and other info the first time they use the app and remember it
+        User user = importFileData(); //TODO: Some way to get the user's name and other info the first time they use the app and remember it
 
-        launchMainWindow();
+        if (user == null) {
+            user = new User("SomeName");
+        } //TODO: implement way to get this from user only on first pass through
 
-        saveAndExit();
+        launchMainWindow(user);
+
+        saveAndExit(user);
     }
 
     private static void displayWelcome(){
         System.out.println(DisplayUtils.getHeader("-- WELCOME TO RECIPE 4 SUCCESS --"));
     }
 
-    private static void importFileData() {
-        //TODO: Import the User data from files
+    private static User importFileData() {
+        return new UserManager().load();
     }
 
-    private static void launchMainWindow(){
-
-        User user = new User("Name"); //TODO: implement way to get this from user only on first pass through
+    private static void launchMainWindow(User user){
+               
         UserController.initUserController(user);
         UserController userController = UserController.getUserController();
         MainMenuController mmController = new MainMenuController(userController);
@@ -35,8 +43,16 @@ public class R4SDriver {
         mmController.launchMainMenu();
     }
 
-    private static void saveAndExit() {
-        //TODO: Save data from application before exit
+    private static void saveAndExit(User user) {
+        try { //TODO: Save data from application before exit
+            new UserManager().save(user);
+            new CategoryPoolManager().save(Category.Pool.getInstance());
+            new FoodItemPoolManager().save(FoodItem.Pool.getInstance());
+        } catch (Exception e) {
+            // TODO tell the user they are SOL
+            System.err.println("An error occured while saving");
+            e.printStackTrace();
+        }
  
         System.out.println("Thank you for using Recipe4Success!");
         System.out.println("\n\tapplication exiting...\n\n");
