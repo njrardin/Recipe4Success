e2d4174c78948eccc9c2400306f78c0b71a4f930
Alex Smith <alex1000242@gmail.com>
2022-05-02 15:44:58 -0500

    added import from method

--- recipe4success/src/main/java/it326/r4s/controller/PorterController.java
+++ recipe4success/src/main/java/it326/r4s/controller/PorterController.java
@@ -3,9 +3,15 @@ package it326.r4s.controller;
 import org.apache.commons.io.FilenameUtils;
 
 import it326.r4s.model.ExporterProducer;
+import it326.r4s.model.ImporterProducer;
 import it326.r4s.model.Portable;
 import it326.r4s.view.PorterView;
 
+/**
+ * Controller for all portable objects.
+ * @author Alex Smith (alsmi14@ilstu.edu)
+ * @date 5/1/22
+ */
 public class PorterController<T extends Portable> {
     private PorterView porterView;
     private Class<T> classT;
@@ -54,7 +60,41 @@ public class PorterController<T extends Portable> {
         }
     }
 
-    public T importFrom() {
+    public T importFrom() { // TODO still need to rebuild the references
+        T imported = null;
+        String importPath = porterView.getImportPath();
 
-    }
+        if (!importPath.equals("")) {
+            String extension = FilenameUtils.getExtension(importPath);
+            importPath = FilenameUtils.removeExtension(importPath);
+            ImporterProducer.Type importType = null;
+
+            // Get the export type.
+            if (!extension.equals("")) {
+                for (ImporterProducer.Type type : ImporterProducer.Type.values()) {
+                    if (extension.equalsIgnoreCase(type.name())) {
+                        importType = type;
+                        break;
+                    }
+                }
+            }
+
+            if (importType != null) {
+                importPath += "." + importType.name().toLowerCase();
+            } else {
+                System.out.println(extension + " is not a supported import file type, exiting ...");
+                return imported;
+            }            
+            
+            try {
+                imported = ImporterProducer.getImporter(importType, classT).importFrom(importPath);
+            } catch (Exception e) {
+                System.err.println("An error occurred while importing.");
+            }
+        } else {
+            System.out.println("No file was selected, exiting ...");
+        }
+        
+        return imported;
+    }    
 }
