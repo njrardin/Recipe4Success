3ab7cc7dd44cb5ae26e2a6d0436b57a7f92fc5d9
Alex Smith <alex1000242@gmail.com>
2022-05-02 16:56:37 -0500

    rough implementation of rebuilding references

--- recipe4success/src/main/java/it326/r4s/controller/PorterController.java
+++ recipe4success/src/main/java/it326/r4s/controller/PorterController.java
@@ -1,10 +1,20 @@
 package it326.r4s.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.apache.commons.io.FilenameUtils;
 
+import it326.r4s.model.Category;
 import it326.r4s.model.ExporterProducer;
+import it326.r4s.model.FoodItem;
 import it326.r4s.model.ImporterProducer;
+import it326.r4s.model.Ingredient;
+import it326.r4s.model.Meal;
+import it326.r4s.model.MealPlan;
 import it326.r4s.model.Portable;
+import it326.r4s.model.Recipe;
+import it326.r4s.model.User;
 import it326.r4s.view.PorterView;
 
 /**
@@ -15,14 +25,16 @@ import it326.r4s.view.PorterView;
 public class PorterController<T extends Portable> {
     private PorterView porterView;
     private Class<T> classT;
+    private UserController userController;
 
-    private PorterController(Class<T> classT) {
+    private PorterController(Class<T> classT, UserController userController) {
         this.porterView = new PorterView();
         this.classT = classT;
+        this.userController = userController;
     }
 
-    public static <S extends Portable> PorterController<S> of(Class<S> type) {
-        return new PorterController<S>(type);
+    public static <S extends Portable> PorterController<S> of(Class<S> type, UserController userController) {
+        return new PorterController<S>(type, userController);
     }
 
     public void exportTo(T portable) {
@@ -60,7 +72,7 @@ public class PorterController<T extends Portable> {
         }
     }
 
-    public T importFrom() { // TODO still need to rebuild the references
+    public T importFrom() {
         T imported = null;
         String importPath = porterView.getImportPath();
 
@@ -86,8 +98,22 @@ public class PorterController<T extends Portable> {
                 return imported;
             }            
             
-            try {
+            try { // This is a really gross implementation, I know, but we are running out of time.
                 imported = ImporterProducer.getImporter(importType, classT).importFrom(importPath);
+                
+                // Rebuild the references.
+                if (imported instanceof Recipe) {
+                    Recipe recipe = (Recipe) imported;
+
+                    rebuildRecipeCategories(recipe);
+                    rebuildIngredients(recipe.getIngredientList().getIngredients());
+                } else if (imported instanceof MealPlan) {
+                    MealPlan mealPlan = (MealPlan) imported;
+
+                    rebuildMeals(mealPlan);
+                } else {
+                    throw new Exception(imported.getClass() + " is not yet a supported portable type.");
+                }
             } catch (Exception e) {
                 System.err.println("An error occurred while importing.");
             }
@@ -96,5 +122,35 @@ public class PorterController<T extends Portable> {
         }
         
         return imported;
+    }
+
+    private void rebuildMeals(MealPlan mealPlan) {
+        User user = userController.getUser();
+
+        for (Meal meal : mealPlan.getMeals()) {
+            for (Recipe recipe : user.getRecipeBook().getRecipes()) {
+                if (meal.getRecipe().getName().equals(recipe.getName())) {
+                    meal.setRecipe(recipe);
+                }
+            }
+        }
+    }
+
+    private void rebuildRecipeCategories(Recipe recipe) {
+        Category.Pool categoryPool = Category.Pool.getInstance();
+        Collection<Category> categories = new ArrayList<>(recipe.getCategories());
+        
+        recipe.clearCategories();
+        for (Category category : categories) {
+            recipe.addCategory(categoryPool.getCategory(Category.Type.RECIPE, category.getName()));
+        }
+    }
+
+    private void rebuildIngredients(Collection<Ingredient> ingredients) {
+        FoodItem.Pool foodItemPool = FoodItem.Pool.getInstance();
+
+        for (Ingredient ingredient : ingredients) {
+            ingredient.setFoodItem(foodItemPool.getFoodItem(ingredient.getFoodItem().getName()));
+        }
     }    
 }
