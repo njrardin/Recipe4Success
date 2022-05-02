03f02c077a79a57b29dfce08c53c7a6f3913113a
Alex Smith <alex1000242@gmail.com>
2022-05-02 15:45:09 -0500

    added header comment

--- recipe4success/src/main/java/it326/r4s/view/PorterView.java
+++ recipe4success/src/main/java/it326/r4s/view/PorterView.java
@@ -4,9 +4,13 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 import it326.r4s.model.ExporterProducer;
-import it326.r4s.model.ImporterProducer;
 import it326.r4s.view.utilities.InputAccess;
 
+/**
+ * View for all portable objects.
+ * @author Alex Smith (alsmi14@ilstu.edu)
+ * @date 5/1/22
+ */
 public class PorterView {
     JFileChooser chooser;
     JFrame frame;    
@@ -56,8 +60,4 @@ public class PorterView {
 
         return ExporterProducer.Type.values()[inputAccess.getOptionSelection("Export", "Which format do you want to export to?", options) - 1];
     }
-
-    // public ImporterProducer.Type getImportType() {
-
-    // }
 }
