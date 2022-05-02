4b7ac9a2bd08c7f7e1ec1239b4c7b816a23ca865
Nate Rardin <nathanaelrardin@gmail.com>
2022-05-02 00:08:27 -0500

    category tests

--- recipe4success/src/test/java/it326/r4s/CategoryTest.java
+++ recipe4success/src/test/java/it326/r4s/CategoryTest.java
@@ -2,6 +2,8 @@ package it326.r4s;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
@@ -9,6 +11,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import it326.r4s.model.Category;
+import it326.r4s.model.Category.Type;
 
 @RunWith (Enclosed.class)
 public class CategoryTest {
@@ -35,6 +38,8 @@ public class CategoryTest {
             pool.getCategory(Category.Type.FOODITEM, "Vegetable");
             pool.getCategory(Category.Type.FOODITEM, "Fruit");
             pool.getCategory(Category.Type.FOODITEM, "Meat");
+            pool.getCategory(Category.Type.RECIPE, "a recipe category");
+            pool.getCategory(Category.Type.RECIPE, "willBeRemoved"); //removed in removeCategory test
         }
     
         @Test
@@ -44,11 +49,35 @@ public class CategoryTest {
         }
 
         @Test
+        public void testGetCategories(){
+            ArrayList<Category> fiCategoryList = new ArrayList<Category>();
+            fiCategoryList.add(new Category("Vegetable"));
+            fiCategoryList.add(new Category("Fruit"));
+            fiCategoryList.add(new Category("Meat"));
+            assertTrue(pool.getCategories(Type.FOODITEM).containsAll(fiCategoryList));
+            fiCategoryList.add(new Category("a recipe category"));
+            assertFalse(pool.getCategories(Type.FOODITEM).containsAll(fiCategoryList));
+        }
+
+        @Test
         public void testGetInstance(){
             Category.Pool samePool = Category.Pool.getInstance();
             assertTrue(pool == samePool); //tests for equality by reference
         }
 
+        @Test
+        public void testRemoveCategory(){
+            assertFalse(pool.removeCategory(Type.FOODITEM, "doesNotExist"));
+            assertFalse(pool.removeCategory(Type.FOODITEM, "willBeRemoved"));
+            assertTrue(pool.removeCategory(Type.RECIPE, "willBeRemoved"));
+        }
+
+        @Test
+        public void testContains(){
+            assertFalse(pool.contains(Type.FOODITEM, "Vegechair"));
+            assertFalse(pool.contains(Type.RECIPE, "Vegetable"));
+            assertTrue(pool.contains(Type.FOODITEM, "Vegetable"));
+        }
         
     }
 }
