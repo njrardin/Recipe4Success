692bef0c219b50ec489158bd9a07f63bce1aa063
Josh Nepomuceno <jnepo@Joshs-MacBook-Pro.local>
2022-05-02 18:48:00 -0500

    finished Delete Recipe whitebox testing

--- recipe4success/src/test/java/it326/WhiteboxTesting/DeleteRecipeWhitebox.java
+++ recipe4success/src/test/java/it326/WhiteboxTesting/DeleteRecipeWhitebox.java
@@ -15,11 +15,37 @@ public class DeleteRecipeWhitebox {
 
         // basis path 1:
         Recipe recipe1 = builder.build();
+        recipe1.setName("test recipe 1");
         ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        recipes.add(recipe1);
 
         RecipeBook recipeBook = new RecipeBook(recipes);
+        System.out.println("\nbasis path 1: before removal");
+        System.out.println(recipeBook.toString());
 
         recipeBook.removeRecipe(recipe1);
+        System.out.println("\nbasis path 1: after removal");
+        System.out.println(recipeBook.toString());
+
+        // basis path 2:
+        recipeBook.addRecipe(recipe1);
+        Recipe recipe2 = builder.build();
+        recipe2.setName("test recipe 2");
+        recipeBook.addRecipe(recipe2);
+        System.out.println("\nbasis path 2: before removal");
+        System.out.println(recipeBook.toString());
+
+        recipeBook.removeRecipe(recipe2);
+        System.out.println("\nbasis path 2: after removal");
+        System.out.println(recipeBook.toString());
+
+        // basis path 3:
+        System.out.println("\nbasis path 3: before removal");
+        System.out.println(recipeBook.toString());
+        recipeBook.removeRecipe(recipe2);
+        System.out.println("\nbasis path 3: after removal");
+        System.out.println(recipeBook.toString());
+        System.out.println();
     }
     
 }
