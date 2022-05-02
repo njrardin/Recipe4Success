7a6e0daf81de79b3a00812f75d74b9557ea7aec5
Alex Smith <alex1000242@gmail.com>
2022-05-02 02:53:26 -0500

    Created view for porting objects

---
+++ recipe4success/src/main/java/it326/r4s/view/PorterView.java
@@ -0,0 +1,63 @@
+package it326.r4s.view;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import it326.r4s.model.ExporterProducer;
+import it326.r4s.model.ImporterProducer;
+import it326.r4s.view.utilities.InputAccess;
+
+public class PorterView {
+    JFileChooser chooser;
+    JFrame frame;    
+    InputAccess inputAccess;
+
+    public PorterView() {
+        chooser = new JFileChooser();
+        chooser.setMultiSelectionEnabled(false);
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        frame = new JFrame("R4S File Porter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setAlwaysOnTop(true);
+
+        inputAccess = new InputAccess();
+    }
+
+    public String getExportPath() {
+        String exportPath = "";
+
+        // Prompt the user for an export path.
+        if (chooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+            exportPath = chooser.getSelectedFile().getAbsolutePath();
+        }
+
+        return exportPath;
+    }
+
+    public String getImportPath() {
+        String importPath = "";
+
+        // Prompt the user for an import path.
+        if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+            importPath = chooser.getSelectedFile().getAbsolutePath();
+        }
+
+        return importPath;
+    }
+
+    public ExporterProducer.Type getExportType() {
+        String[] options = new String[ExporterProducer.Type.values().length];
+
+        int i = 0;
+        for (ExporterProducer.Type type : ExporterProducer.Type.values()) {
+            options[i++] = type.name();
+        }
+
+        return ExporterProducer.Type.values()[inputAccess.getOptionSelection("Export", "Which format do you want to export to?", options) - 1];
+    }
+
+    // public ImporterProducer.Type getImportType() {
+
+    // }
+}
