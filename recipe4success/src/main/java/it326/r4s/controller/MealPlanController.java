7a814a785b5c6b807d92ae3eb38da3bd58abccf9
Nate Rardin <nathanaelrardin@gmail.com>
2022-05-02 21:37:34 -0500

    fixed issue

--- recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
+++ recipe4success/src/main/java/it326/r4s/controller/MealPlanController.java
@@ -128,7 +128,7 @@ public class MealPlanController  {
     public void removeRecipeFromMealPlan() {
         System.out.println("Which recipe would you like to remove?");
         // TODO this is issue #5 on the trello, also needs a try catch
-        RecipeController selectedRecipeController = authorController.getRecipeBookController().selectRecipeController();
+        RecipeController selectedRecipeController = authorController.getRecipeBookController().selectRecipeController(getRecipeControllers());
 
         mealPlan.removeMeal(selectedRecipeController.getRecipe());
 
